dcf12ba167c1bfb5d142ccd865c9117cd7c9b092
Val Currie <contact@valcuri.email>
2021-12-22 02:23:15 -0600

    wrapper methods so it's easy to add db backends
    I'm winding down after my first public PR
    
    it was to github.com/1set/todotxt ahhhhhhhh
    
    omgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomgomg

--- keepalive-api/db/connection.go
+++ keepalive-api/db/connection.go
@@ -22,27 +22,35 @@ func Connect(basePath string) *Connection {
 	return &Connection{diskv: dv}
 }
 
-/*
- * TODO:
- *		try recovering from a panic occuring in a transform function
- *		and return an appropriate error. If that works, all methods*
- *		must implement a deferred recovery that propogrates an error.
- *
- *		* it is okay to omit the error if the method does not invoke
- *			a transform or it's inverse AND does not have another
- *			error to propogate up to the method caller.
- *
+func (c *Connection) ReadTodos(username string) (todotxt.TaskList, error) {
+	return c.readTodosDiskv(username)
+}
+
+func (c *Connection) WriteTodos(username string, todoList todotxt.TaskList) error {
+	return c.writeTodosDiskv(username, todoList)
+}
+
+/* TODO: ====================== diskv specific  =====================\
+ * |	try recovering from a panic occuring in a transform function |
+ * |	and return an appropriate error. If that works, all methods* |
+ * |	must implement a deferred recovery that propogrates an error.|
+ * |                                                                 |
+ * |	* it is okay to omit the error if the method does not invoke |
+ * |		a transform or it's inverse AND does not have another    |
+ * |		error to propogate up to the method caller.              |
+ * \=================================================================/
  */
 
-func (c *Connection) ReadTodos(username string) (todotxt.TaskList, error) {
+func (c *Connection) readTodosDiskv(username string) (todotxt.TaskList, error) {
 	allTasks, err := c.diskv.ReadStream("keepalive/todos/"+username+"/todo", false)
-	defer allTasks.Close()
+	defer allTasks.Close() // ReadStream returns a ReadCloser (has .Read() and .Close())
 	if err != nil {
 		return nil, err
 	}
 
 	todoList := todotxt.NewTaskList()
 	// LoadFromFile converts the "file" to a bufio.Scanner so any io.Reader is fine
+	//		however, todotxt requires *os.File specifically so we're asserting that
 	err = todoList.LoadFromFile(allTasks.(*os.File))
 	if err != nil {
 		return nil, err
@@ -51,7 +59,7 @@ func (c *Connection) ReadTodos(username string) (todotxt.TaskList, error) {
 	return todoList, nil
 }
 
-func (c *Connection) WriteTodos(username string, todoList todotxt.TaskList) error {
+func (c *Connection) writeTodosDiskv(username string, todoList todotxt.TaskList) error {
 	if err := c.diskv.WriteString(
 		"keepalive/todos/"+username+"/todo", todoList.String(),
 	); err != nil {
