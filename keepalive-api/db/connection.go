02aaaa6a1e720fed8c668f8392dfb227edd92013
Val Currie <contact@valcuri.email>
2021-12-21 20:26:49 -0600

    added todolist read and write methods

--- keepalive-api/db/connection.go
+++ keepalive-api/db/connection.go
@@ -1,6 +1,10 @@
 package db
 
 import (
+	"bufio"
+	"strings"
+
+	"github.com/1set/todotxt"
 	"github.com/peterbourgon/diskv/v3"
 )
 
@@ -29,3 +33,33 @@ func Connect(basePath string) *Connection {
  *			a transform or it's inverse AND does not have another
  *			error to propogate up to the method caller.
  */
+
+func (c *Connection) ReadTodos(username string) (todotxt.TaskList, error) {
+	allTasks := c.diskv.ReadString("keepalive/todos/" + username + "/todo")
+	taskScanner := bufio.NewScanner(strings.NewReader(allTasks))
+
+	todoList := todotxt.NewTaskList()
+
+	for taskScanner.Scan() {
+		task, err := todotxt.ParseTask(taskScanner.Text())
+		if err != nil {
+			return nil, err
+		}
+		todoList.AddTask(task)
+	}
+
+	return todoList, nil
+}
+
+func (c *Connection) WriteTodos(username string, todoList todotxt.TaskList) error {
+	err := c.diskv.WriteString(
+		"keepalive/todos/"+username+"/todo",
+		todoList.String(),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
