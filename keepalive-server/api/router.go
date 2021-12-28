f78a20f860606866d6e0d7be4c53cf16da9b616f
Val Currie <contact@valcuri.email>
2021-12-28 00:55:19 -0600

    good progress on the router abstraction

---
+++ keepalive-server/api/router.go
@@ -0,0 +1,65 @@
+package api
+
+import (
+	"github.com/casbin/casbin/v2"
+	"github.com/gin-contrib/authz"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type GroupConfig struct {
+	CORSConfig cors.Config
+	Enforcer   *casbin.Enforcer
+	BasePath   string
+}
+
+type API struct {
+	Router       *gin.Engine
+	Groups       map[string]*gin.RouterGroup
+	GroupConfigs map[string]GroupConfig
+}
+
+func NewAPI(opts ...func(*API)) *API {
+	api := API{
+		Router:       gin.New(),
+		Groups:       make(map[string]*gin.RouterGroup),
+		GroupConfigs: make(map[string]GroupConfig),
+	}
+
+	// TODO: logger
+
+	// enable panic recovery
+	api.Router.Use(gin.Recovery())
+
+	// add handlers from config options
+	for _, opt := range opts {
+		opt(&api)
+	}
+
+	return &api
+}
+
+func (api *API) groupMustExist(group string) {
+	// TODO: if group does not exist, create it
+}
+
+func BasePath(group, path string) func(*API) {
+	return func(api *API) {
+		api.groupMustExist(group)
+		// TODO: set the base path
+	}
+}
+
+func AuthzEnforce(group string, enforcer *casbin.Enforcer) func(*API) {
+	return func(api *API) {
+		api.groupMustExist(group)
+		api.Groups[group].Use(authz.NewAuthorizer(enforcer))
+	}
+}
+
+func CORS(group string, config cors.Config) func(*API) {
+	return func(api *API) {
+		api.groupMustExist(group)
+		api.Groups[group].Use(cors.New(config))
+	}
+}
--- keepalive-server/go.mod
+++ keepalive-server/go.mod
@@ -4,10 +4,31 @@ go 1.17
 
 require (
 	github.com/1set/todotxt v0.0.4
+	github.com/casbin/casbin/v2 v2.40.6
+	github.com/gin-contrib/authz v0.0.0-20211106112157-2a70df09d207
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.7.7
 	github.com/peterbourgon/diskv/v3 v3.0.1
 )
 
 require (
 	github.com/1set/gut v0.0.0-20201117175203-a82363231997 // indirect
+	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/btree v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
