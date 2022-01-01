6fa115dfdcfb66cf25f0d34c56e9e0b54d75ad7c
Val Currie <contact@valcuri.email>
2022-01-01 00:23:03 -0600

    simplified gin middleware config stuff down to a single struct and documented

--- keepalive-server/api/router.go
+++ keepalive-server/api/router.go
@@ -7,53 +7,63 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type API struct {
-	Router *gin.Engine
-	Groups map[string]*gin.RouterGroup
+// Config is used internally before the API is returned to New's caller
+//		This enables chaining together configuration functions for things
+//		like multiple middlewares on subroutes without *needing* access
+//		to the RouterGroups. You do get the map used to pull this off
+//		back as a second return value. This is useful for trying out new
+//		configurations outside the module before needing to try to write
+//		a generally useful higher order configuration function.
+type config struct {
+	groups map[string]*gin.RouterGroup
+	engine *gin.Engine
 }
 
-// NewAPI implements the functional config of a router via a wrapper
+// NewEngine implements the functional config of a router via a wrapper
 //		All RouterGroups are created via config functions. All config
 //		functions take a group basePath as a first parameter.
-func NewAPI(opts ...func(*API)) *API {
-	api := API{
-		Router: gin.New(),
-		Groups: make(map[string]*gin.RouterGroup),
+//		Also, it returns a pointer to the map of router groups
+//			for further tweaking, for now.
+func NewEngine(opts ...func(*config)) (*gin.Engine, map[string]*gin.RouterGroup) {
+	c := config{
+		groups: make(map[string]*gin.RouterGroup),
+		engine: gin.New(),
 	}
 
 	// TODO: logger
 
 	// enable panic recovery
-	api.Router.Use(gin.Recovery())
+	c.engine.Use(gin.Recovery())
 
 	// add handlers from config options
 	for _, opt := range opts {
-		opt(&api)
+		opt(&c)
 	}
 
-	return &api
+	return c.engine, c.groups
 }
 
-func (api *API) groupMustExist(group string) {
-	if _, exists := api.Groups[group]; !exists {
-		api.Groups[group] = api.Router.Group(group)
+// . makes sure that the RouterGroup you're trying to configure exists
+func (c *config) groupMustExist(group string) {
+	if _, exists := c.groups[group]; !exists {
+		c.groups[group] = c.engine.Group(group)
 	}
 }
 
-// AuthzEnforce attaches a casbin Enforcer (via a reference) to a RouterGroup
+// AuthzEnforce attaches pointer to a casbin Enforcer to a RouterGroup
 //		specified by it's basePath/name
-func AuthzEnforce(group string, enforcer *casbin.Enforcer) func(*API) {
-	return func(api *API) {
-		api.groupMustExist(group)
-		api.Groups[group].Use(authz.NewAuthorizer(enforcer))
+func AuthzEnforce(group string, enforcer *casbin.Enforcer) func(*config) {
+	return func(c *config) {
+		c.groupMustExist(group)
+		c.groups[group].Use(authz.NewAuthorizer(enforcer))
 	}
 }
 
 // CORS attaches a cors configuration struct to a RouterGroup
 //		specified by it's basePath/name
-func CORS(group string, config cors.Config) func(*API) {
-	return func(api *API) {
-		api.groupMustExist(group)
-		api.Groups[group].Use(cors.New(config))
+func CORS(group string, corsConfig cors.Config) func(*config) {
+	return func(c *config) {
+		c.groupMustExist(group)
+		c.groups[group].Use(cors.New(corsConfig))
 	}
 }
